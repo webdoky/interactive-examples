413177bf4c8d956410bdefc9e3b6aad3f01551cd
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2022-10-11 10:11:56 +0300

    Update samples

--- live-examples/js-examples/object/object-assign.js
+++ live-examples/js-examples/object/object-assign.js
@@ -6,5 +6,5 @@ const returnedTarget = Object.assign(target, source);
 console.log(target);
 // очікуваний вивід: Object { a: 1, b: 4, c: 5 }
 
-console.log(returnedTarget);
-// очікуваний вивід: Object { a: 1, b: 4, c: 5 }
+console.log(returnedTarget === target);
+// очікуваний вивід: true
