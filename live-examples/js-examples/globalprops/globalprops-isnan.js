d4e3c8ee5014413cac80c6f2ddcbb5400d26a5c5
Yevhen Stuzhuk <bareust@gmail.com>
2025-01-07 08:54:21 +0300

    Apply suggestions from code review
    Co-authored-by: Vitalii Perehonchuk <brute18@gmail.com>

--- live-examples/js-examples/globalprops/globalprops-isnan.js
+++ live-examples/js-examples/globalprops/globalprops-isnan.js
@@ -1,12 +1,12 @@
 function milliseconds(x) {
   if (isNaN(x)) {
-    return 'Not a Number!';
+    return 'Не число!';
   }
   return x * 1000;
 }
 
 console.log(milliseconds('100F'));
-// Очікуваний результат: "Not a Number!"
+// Очікуваний результат: "Не число!"
 
 console.log(milliseconds('0.0314E+2'));
 // Очікуваний результат: 3140
